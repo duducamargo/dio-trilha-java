3a239a1077911774ad98013cc12113aede429db8
Eduardo Farias Camargo <edu.farias.camargo@gmail.com>
2024-04-26 21:13:55 -0300

    Projeto ContaBanco foi finalizado

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,121 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ContaTerminal {
+
+    public static void imprimirMenu() {
+        System.out.println("[1] Criar Conta");
+        System.out.println("[2] Entrar na conta");
+        System.out.println("[3] Sair");
+    }
+
+    public static void menuConta() {
+        System.out.println("[1] Listar conta");
+        System.out.println("[2] Realizar depósito");
+        System.out.println("[3] Realizar saque");
+        System.out.println("[4] Excluir conta");
+        System.out.println("[5] Sair");
+    }
+
+    public static void criarConta(List<ContasBanco> contas, Scanner scanner) {
+        String nomeCliente, agencia;
+        int numeroConta;
+        double saldo = 0;
+
+        System.out.print("Insira o seu nome: ");
+        nomeCliente = scanner.nextLine();
+
+        System.out.print("Insira o numero de sua agencia: ");
+        agencia = scanner.nextLine();
+
+        System.out.print("Insira o numero de sua conta: ");
+        numeroConta = Integer.parseInt(scanner.nextLine());
+
+        ContasBanco conta = new ContasBanco(nomeCliente, numeroConta, agencia, saldo);
+
+        contas.add(conta);
+
+        System.out.println("Conta criada com sucesso!");
+    }
+
+    public static void entrarConta(List<ContasBanco> contas, Scanner scanner) {
+        int numeroConta, opc;
+        boolean encontrado = false;
+
+        System.out.print("Insira o numero de sua conta: ");
+        numeroConta = Integer.parseInt(scanner.nextLine());
+
+        for(int i = 0; i < contas.size(); i++) {
+            if(contas.get(i).getNumeroConta() == numeroConta) {
+                encontrado = true;
+                do {
+                    menuConta();
+                    opc = Integer.parseInt(scanner.nextLine());
+
+                    switch (opc) {
+                        case 1:
+                            System.out.println("Nome: " + contas.get(i).getNomeCliente());
+                            System.out.println("Número da conta: " + contas.get(i).getNumeroConta());
+                            System.out.println("Agência: " + contas.get(i).getAgencia());
+                            System.out.println("Saldo: " + contas.get(i).getSaldo());
+                            break;
+                        case 2:
+                            System.out.print("Insira o valor que você deseja depositar em sua conta: ");
+                            double valorDepositado = Double.parseDouble(scanner.nextLine());
+                            contas.get(i).depositar(valorDepositado);
+                            System.out.println("Depósito realizado, seu saldo é: " + contas.get(i).getSaldo());
+                            break;
+                        case 3:
+                            System.out.print("Insira o valor que você deseja sacar de sua conta: ");
+                            double valorSacado = Double.parseDouble(scanner.nextLine());
+                            contas.get(i).sacar(valorSacado);
+                            System.out.println(("Saque realizado com sucesso, seu saldo é: " + contas.get(i).getSaldo()));
+                            break;
+                        case 4:
+                            System.out.println("Excluindo conta...");
+                            contas.remove(i);
+                            return;
+                        case 5:
+                            System.out.println("Saindo...");
+                        default:
+                            System.out.println("Opção inválida!");
+                            break;
+                    }
+                }while(opc != 5);
+            }
+        }
+        if(!encontrado) {
+            System.out.println("A conta informada não foi encontrada!");
+        }
+    }
+
+    public static void main(String[] args)  {
+        Scanner scanner = new Scanner(System.in);
+        List <ContasBanco> contas = new ArrayList<ContasBanco>();
+        int opc;
+
+        do{
+            imprimirMenu();
+            opc = Integer.parseInt(scanner.nextLine());
+
+            switch (opc) {
+                case 1:
+                    criarConta(contas, scanner);
+                    break;
+                case 2:
+                    entrarConta(contas, scanner);
+                    break;
+                case 3:
+                    System.out.println("Até Mais!");
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+                    break;
+            }
+        }while(opc != 3);
+        
+        
+        scanner.close();
+    }
+}
